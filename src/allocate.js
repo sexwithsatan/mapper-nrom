6f56ee8f0b414b48e704e0874a8c920477349ce9
sex with satan <sexwithsatan@users.sf.net>
2018-11-10 15:43:58 -0800

    WIP

--- src/allocate.js
+++ src/allocate.js
@@ -5,25 +5,43 @@ export default
 async function allocate(rom) {
   const data = await uploadAsArrayBuffer(rom)
   const header = new Uint8ClampedArray(data, 0, 16)
-  const {size: {PRG, CHR}} = deserialize(header)
-  const prgrom = new Uint8ClampedArray(data, 16, PRG)
-  const chrrom = new Uint8ClampedArray(data, 16 + PRG, CHR)
+  const {size} = deserialize(header)
+
+  const prgrom = new Uint8ClampedArray(data, 16, size.program)
+  const chrrom = new Uint8ClampedArray(data, 16 + size.program, size.graphics)
+
+  const banks = {
+    program: [
+      prgrom.subarray(0, 0x4000),
+      prgrom.subarray(size.program - 0x4000, size.program)
+    ],
+    graphics: chrrom.subarray(0, 0x2000)
+  }
+
+  console.log(deserialize(header))
 
   return {
     program: {
-      read(AL, AH) {
-        return prgrom[(AL + (AH << 8)) & 0x3fff]
+      read(al, ah) {
+        const a14 = (ah >> 6) & 1
+
+        // A14 of the address selects a 16-KiB PRG-ROM bank
+        // A14=0 selects the lower bank $8000-BFFF
+        // A14=1 selects the upper bank $C000-FFFF
+        return banks.program[a14][al + (ah << 8)]
       },
-      write(AL, AH, D) {
+
+      write(al, ah, d) {
         // TODO
       }
     },
 
     graphics: {
-      read(AD, A) {
-        return chrrom((AD + (A << 8)) & 0x1fff]
+      read(al, ah) {
+        return banks.graphics[al + (ah << 8)]
       },
-      write(AD, A, D) {
+
+      write(al, ah, d) {
         // TODO
       }
     }
