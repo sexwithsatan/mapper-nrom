8d5d84cb01884f055a4e22340b4d98411bc3ad29
sex with satan <sexwithsatan@users.sf.net>
2018-11-13 14:47:30 -0800

    WIP

--- src/allocate.js
+++ src/allocate.js
@@ -13,7 +13,7 @@ async function allocate(rom) {
   const banks = {
     program: [
       prgrom.subarray(0, 0x4000),
-      prgrom.subarray(size.program - 0x4000, size.program)
+      prgrom.subarray(prgrom.length - 0x4000, prgrom.length)
     ],
     graphics: chrrom.subarray(0, 0x2000)
   }
@@ -25,9 +25,9 @@ async function allocate(rom) {
       read(al, ah) {
         const a14 = (ah >> 6) & 1
 
-        // A14 of the address selects a 16-KiB PRG-ROM bank
-        // A14=0 selects the lower bank $8000-BFFF
-        // A14=1 selects the upper bank $C000-FFFF
+        // A14 of the address bus selects a 16-KiB ROM bank
+        // A14=0 selects lower PRG-ROM $8000-BFFF
+        // A14=1 selects upper PRG-ROM $C000-FFFF
         return banks.program[a14][al + (ah << 8) & 0x3fff]
       },
 
@@ -37,11 +37,11 @@ async function allocate(rom) {
     },
 
     graphics: {
-      read(al, ah) {
-        return banks.graphics[al + (ah << 8) & 0x1fff]
+      read(ad, a) {
+        return banks.graphics[ad + (a << 8) & 0x1fff]
       },
 
-      write(al, ah, d) {
+      write(ad, a, d) {
         // TODO
       }
     }
