5ed1fe8ae890c60cb885994c0190193b45d28adc
sex with satan <sexwithsatan@users.sf.net>
2018-12-04 12:45:58 -0800

    modified:   src/allocate.js

--- src/allocate.js
+++ src/allocate.js
@@ -12,10 +12,14 @@ async function allocate(rom) {
 
   const banks = {
     program: [
-      prgrom.subarray(0, 0x4000),
-      prgrom.subarray(size.program - 0x4000, size.program)
+/* CPU $8000 */prgrom.subarray(0, 0x4000),
+/* CPU $C000 */prgrom.subarray(size.program - 0x4000, size.program)
     ],
-    graphics: chrrom.subarray(0, 0x2000)
+
+    graphics: [
+/* PPU $0000 */chrrom.subarray(0x0000, 0x1000),
+/* PPU $1000 */chrrom.subarray(0x1000, 0x2000)
+    ]
   }
 
   console.log(deserialize(header))
@@ -25,9 +29,9 @@ async function allocate(rom) {
       read(al, ah) {
         const a14 = (ah >> 6) & 1
 
-        // A14 of the address selects a 16-KiB PRG-ROM bank
-        // A14=0 selects the lower bank $8000-BFFF
-        // A14=1 selects the upper bank $C000-FFFF
+        // A14 of the address selects a 16-KiB PRG-ROM bank:
+        //  A14=0 selects the lower bank $8000-$BFFF
+        //  A14=1 selects the upper bank $C000-$FFFF
         return banks.program[a14][al + (ah << 8) & 0x3fff]
       },
 
@@ -38,7 +42,12 @@ async function allocate(rom) {
 
     graphics: {
       read(al, ah) {
-        return banks.graphics[al + (ah << 8) & 0x1fff]
+        const a12 = (ah >> 4) & 1
+        
+        // A12 of the address selects one half of the pattern table:
+        //  A12=0 selects the left half $0000-$0FFF
+        //  A12=1 selects the right half $1000-$1FFF
+        return banks.graphics[a12][al + (ah << 8) & 0x0fff]
       },
 
       write(al, ah, d) {
